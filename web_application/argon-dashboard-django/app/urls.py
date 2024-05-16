7c04b1e835328a2db5c368d0b01dffe9e9d557aa
adriano22_ <califano993@gmail.com>
2024-05-16 18:43:48 +0200

    Added new results visualization
    In app/urls.py:
    - added path for new file-smells page.
    
    In app/views.py:
    - changed saved function for retrieving only user file_names.
    - created file_smells function for extracting smells and calculating DQDs.
    - added function for the calculation of completeness/uniqueness/validity.
    
    Added saved.html for the new visualization.
    Changed saved-results.html with the new ui.
    Uploaded a mock_dataset for testing.

--- web_application/argon-dashboard-django/app/urls.py
+++ web_application/argon-dashboard-django/app/urls.py
@@ -9,18 +9,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-
     path('', views.upload, name='upload'),
-
     re_path('customize.html', views.customize, name='customize'),
-
     re_path('results.html', views.result, name='result'),
-
     re_path('saved.html', views.saved, name='saved'),
-    
+    re_path('filesmells', views.file_smells),
     re_path(r'^.*\.*', views.pages, name='pages'),
-
-
 ]
 
 if settings.DEBUG:
